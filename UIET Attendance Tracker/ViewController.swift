ec137eaf274b179fe8d30b77f6c3951493a59780
harshitjindal <harshit.jindal@icloud.com>
2019-07-16 20:17:07 +0530

    Initial Commit

---
+++ UIET Attendance Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIET Attendance Tracker
+//
+//  Created by Harshit Jindal on 16/07/19.
+//  Copyright © 2019 Harshit Jindal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
