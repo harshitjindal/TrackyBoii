614251e24a19a6d68afadaf4196a33c876d9344e
harshitjindal <harshit.jindal@icloud.com>
2019-10-01 16:51:22 +0530

    Fix Notifications Issue

--- UIET Attendance Tracker/AppDelegate.swift
+++ UIET Attendance Tracker/AppDelegate.swift
@@ -12,12 +12,12 @@ import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-
+    
     var window: UIWindow?
     
     let notificationCenter = UNUserNotificationCenter.current()
     let content = UNMutableNotificationContent()
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         do {
@@ -30,31 +30,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let notificationsRegisteredKey = "enabled"
         let defaults = UserDefaults.standard
         var notificationsRegistered = defaults.bool(forKey: notificationsRegisteredKey)
-        if !notificationsRegistered {
-            defaults.set(true, forKey: notificationsRegisteredKey)
-        }
         
         if notificationsRegistered {
-            print("Notifications Registered")
+            print("Notifications Already Registered")
+        } else {
             registerDailyNotifications()
+            defaults.set(true, forKey: notificationsRegisteredKey)
         }
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         UIApplication.shared.applicationIconBadgeNumber = 0
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
     }
     
@@ -64,38 +63,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let options: UNAuthorizationOptions = [.alert, .sound, .badge]
         notificationCenter.requestAuthorization(options: options) {
             (didAllow, error) in
-            if !didAllow {
+            if didAllow {
+                self.content.title = "Daily Attendance Reminder"
+                self.content.body = "Please Mark Your Attendance"
+                
+                // Configure the recurring date.
+                var dateComponents = DateComponents()
+                dateComponents.calendar = Calendar.current
+                
+                dateComponents.hour = 17    // 17:00 hours
+                //        let dateMatching = Calendar.current.dateComponents([.weekday, .hour], from: DateComponents(hour: 17))
+                
+                // Create the trigger as a repeating event.
+                let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents,
+                                                            repeats: true)
+                
+                // Create the request
+                let uuidString = UUID().uuidString
+                let request = UNNotificationRequest(identifier: uuidString,
+                                                    content: self.content,
+                                                    trigger: trigger)
+                
+                // Schedule the request with the system.
+                let notificationCenter = UNUserNotificationCenter.current()
+                notificationCenter.add(request) { (error) in
+                    if error != nil {
+                        print(error!.localizedDescription)
+                    }
+                }
+                print("Notification Registered")
+                
+            } else {
                 print("User has declined notifications")
             }
         }
-        
-        content.title = "Daily Attendance Reminder"
-        content.body = "Please Mark Your Attendance"
-        
-        // Configure the recurring date.
-        var dateComponents = DateComponents()
-        dateComponents.calendar = Calendar.current
-        
-        dateComponents.hour = 17    // 17:00 hours
-        //        let dateMatching = Calendar.current.dateComponents([.weekday, .hour], from: DateComponents(hour: 17))
-        
-        // Create the trigger as a repeating event.
-        let trigger = UNCalendarNotificationTrigger(
-            dateMatching: dateComponents, repeats: true)
-        
-        // Create the request
-        let uuidString = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuidString,
-                                            content: content, trigger: trigger)
-        
-        // Schedule the request with the system.
-        let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.add(request) { (error) in
-            if error != nil {
-                print(error!.localizedDescription)
-            }
-        }
     }
-
-
+    
+    
 }
